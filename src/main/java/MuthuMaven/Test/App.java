ebe536fdc9b499c3262617f4bcb105096f633a02
muthu <mpmuthu59@gmail.com>
2020-01-26 19:25:19 +0530

    This is Maven Build

---
+++ src/main/java/MuthuMaven/Test/App.java
@@ -0,0 +1,13 @@
+package MuthuMaven.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MuthuMaven/Test/Tc1.java
@@ -0,0 +1,14 @@
+package MuthuMaven.Test;
+
+import org.testng.annotations.Test;
+
+import junit.framework.TestCase;
+
+public class Tc1  {
+	
+	@Test
+	public void muthu()
+	{
+		System.out.println("Muthu");
+	}
+}
